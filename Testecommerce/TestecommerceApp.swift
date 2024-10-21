a516baedb67ddb5a3184530bd4e584e43ac44017
Lucas Troteseil <troteseil.lucas@gmail.com>
2024-10-21 12:24:11 +0200

    Initial Commit

---
+++ Testecommerce/TestecommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestecommerceApp.swift
+//  Testecommerce
+//
+//  Created by Lucas Pro on 21/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestecommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestecommerceTests/TestecommerceTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestecommerceTests.swift
+//  TestecommerceTests
+//
+//  Created by Lucas Pro on 21/10/2024.
+//
+
+import Testing
+@testable import Testecommerce
+
+struct TestecommerceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
